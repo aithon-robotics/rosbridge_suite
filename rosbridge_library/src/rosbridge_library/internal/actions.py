be03d3591aec3a932465b4d38302df0c6c509f99
Spir0u <28966599+Spir0u@users.noreply.github.com>
2025-10-28 14:08:13 +0100

    add server timeout exception

--- rosbridge_library/src/rosbridge_library/internal/actions.py
+++ rosbridge_library/src/rosbridge_library/internal/actions.py
@@ -171,7 +171,8 @@ class SendGoal:
 
         self.result = None
         client: ActionClient = ActionClient(node_handle, action_class, action_name)
-        client.wait_for_server(timeout_sec=self.server_timeout_time)
+        if( not client.wait_for_server(timeout_sec=self.server_timeout_time)):
+           raise Exception("No action server available")
         send_goal_future = client.send_goal_async(inst, feedback_callback=feedback_cb)
         send_goal_future.add_done_callback(self.goal_response_cb)
 
